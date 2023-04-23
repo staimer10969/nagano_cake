a283fe22adbb7f2f224b3e9fd1ce4ba3794407e3
staimer10969 <@users.noreply.github.com>
2023-04-23 12:51:41 +0000

    first_commit

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+  sessions: "admin/sessions"
+}
+ resources :admin, only: [:new, :index, :show, :edit, :create, :update, :destroy]
+
+  devise_for :customers, skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+ resources :public, only: [:new, :index, :show, :edit, :create, :update, :destroy]
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
