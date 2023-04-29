646cbb040816217be11f4b27bc6b244e81d15874
staimer10969 <@users.noreply.github.com>
2023-04-29 12:50:27 +0000

    4th_commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,26 @@
 Rails.application.routes.draw do
- 
+
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
   sessions: "admin/sessions"
 }
- resources :admin, only: [:top, :new, :show, :edit, :create, :update, :destroy]
+  get "admin" => "homes#top", as:'admin'
+  namespace :admin do
+   resources :items, only: [:index, :new, :create, :show, :edit, :update]
+   resources :customers, only: [:index, :show, :edit, :update]
+   resources :orders, only: [:show]
+  end
 
   devise_for :customers, skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
- resources :public, only: [:new, :index, :show, :edit, :create, :update, :destroy]
-
+  scope module: :public do
+   root to: 'homes#top'
+   get "about" => "homes#about", as:'about'
+   get "customers/infomation" => "customers#show"
+   resources :items, only: [:index, :show]
+   resources :cart_items, only: [:index, :update, :create, :destroy]
+   resources :orders, only: [:new, :create, :index, :show]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
