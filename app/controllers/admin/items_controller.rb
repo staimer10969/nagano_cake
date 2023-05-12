80e8dcac28586cb86fcf1517f680076087b8f6f2
staimer10969 <@users.noreply.github.com>
2023-05-12 14:56:55 +0000

    13th_commit

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -30,7 +30,7 @@ class Admin::ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:image, :name, :introduction, :price)#
+    params.require(:item).permit(:image, :name, :introduction, :price)
   end
 
 end
--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,9 +1,16 @@
 class Public::HomesController < ApplicationController
 
   def top
+    @items = Item.all
+    @items = Item.order('id DESC').limit(4)
   end
 
   def about
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :price)
   end
 
 end
